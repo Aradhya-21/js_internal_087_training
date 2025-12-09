0cfffbfd68203e0293e2aabc9cfc7577aa406bfb
Aradhya Srivastava <asktoaradhya@gmail.com>
2025-12-09 11:48:43 +0530

    Add files via upload

---
+++ day 2/day2.js
@@ -0,0 +1,46 @@
+/*let a=23;
+const b="abc";
+var c =78 
+d=88;
+
+a=34;
+b="bye";
+c=89;
+d=91;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);*/
+
+
+
+/*let a="abc";
+let b="";
+let c=null;
+let d;
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));*/
+
+
+
+let score = "33abc";
+
+
+/*two ways of checking the data type of a variable*/
+console.log(typeof(score));
+console.log(typeof score);        
+
+a=Number(score);
+console.log(typeof a);
+console.log(a);
